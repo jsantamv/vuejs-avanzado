9d569ab16d48e1d58c10ca211f82a0d59af1eab7
Juan Carlos Santamaria V <jcsantamariav@instacredit.com>
2019-05-06 10:27:40 -0600

    actualizacion de fuente

---
+++ Inicio/main.js
@@ -0,0 +1,64 @@
+/* Funcion para aplicar filtros */
+function aplicarfiltros() {
+    let app = new Vue({
+        el: '#app',
+        data: {
+            titulo: 'yo manejando',
+            subtituslos: 'estas viendo el cursp de vue',
+            nombre: '',
+            cursosSelecionados: [],
+            cursos: [
+                {
+                    nombre: 'Curso de Vue.js',
+                    value: 'vue'
+                },
+                {
+                    nombre: 'Curso de seo',
+                    value: 'seo'
+                },
+                {
+                    nombre: 'Curso de react',
+                    value: 'react'
+                },
+                {
+                    nombre: 'Curso de growth',
+                    value: 'growth'
+                },
+            ]
+        },
+        filters: {
+            uppercase: str => str.toUpperCase(),
+            lowercase: str => str.toLowerCase(),
+            capitalize: function (str) {
+                if (typeof str !== 'string') return ''
+                return str.charAt(0).toUpperCase() + str.slice(1)
+            }
+
+        },
+        methods: {
+            submit: function () {
+                console.log('Ejecuta el metodo submit')
+                console.log(this.cursosSelecionados)
+            }
+        }
+    })
+}
+
+/*probando*/
+
+function contador() {
+    let app = new Vue({
+        el: '#app',
+        data: {
+            contador: 0
+        },
+        methods: {
+            sumar: function () {
+                this.contador++
+            },
+            restar: function () {
+                this.contador--
+            }
+        }
+    })
+}
