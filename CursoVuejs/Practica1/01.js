3816adad931ba965bd950b082268193f2287827a
Juan Carlos Santamaria Villegas <jsantamv@live.com>
2019-09-22 20:15:27 -0600

    Actualizacion del Primer curso

---
+++ CursoVuejs/Practica1/01.js
@@ -0,0 +1,13 @@
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hola como estas',
+        frutas: ['Manzana', 'Peras', 'Naranjas', 'Mandarinas'],
+        objetos: [
+            { nombre: 'Juan K', cedula: '205530947', estado: 'falso', anio:15},
+            { nombre: 'Yess', cedula: '109530714', estado: 'falso', anio:20 },
+            { nombre: 'Susuna', cedula: '701230474', estado: 'falso', anio:30 }
+        ]
+    },
+})
