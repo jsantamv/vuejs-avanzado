cbe038c5741748b6482ab93d112d4ce7a1ff57d1
Juan Carlos Santamaria Villegas <jsantamv@live.com>
2019-09-24 18:05:14 -0600

    actualizacion del codigo fuente

---
+++ CursoVuejs/3_ComputadasObservadores/app.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+
+    el: '#app',
+    data: {
+        mensaje: 'Hola soy junakita pero que bonito es VUE',
+        contador: 0
+    },
+    computed: {
+        invertido() {
+            return this.mensaje.split('').reverse().join('')
+        },
+        color() {
+            return {
+                'bg-success': this.contador <= 10,
+                'bg-warning': this.contador > 10 && this.contador < 20,
+                'bg-danger': this.contador >= 20,
+            }
+        }
+    },
+})
