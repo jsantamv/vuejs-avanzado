a69fc490562557ebdf705eda3077e271398de207
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-02-27 14:48:33 -0600

    actualizar

--- 01-breakingBad/src/App.vue
+++ 01-breakingBad/src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+import { ref, defineComponent } from 'vue';
+const counter = ref(0)
+
 </script>
 
 <template>
   <div class="wrapper">
     <h1>Hola</h1>
-    <p>mundo</p>
+    <p>Contador: {{ counter }}</p>
+    <button @click="counter++">+1</button>
   </div>
 </template>
 
@@ -14,3 +18,4 @@
   flex-direction: column;
 }
 </style>
+
