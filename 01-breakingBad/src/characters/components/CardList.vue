ae760f3e1626e0c8a2bddc9298582ad7b6d9012a
Juan Carlos Santamaría Villegas <jsantamv@gmail.com>
2023-03-26 20:51:09 -0600

    Clase 33 - Implemtacion del UseQuery

--- 01-breakingBad/src/characters/components/CardList.vue
+++ 01-breakingBad/src/characters/components/CardList.vue
@@ -1,19 +1,36 @@
 <script setup lang="ts">
+import { breakingBadApi, rickAndMortyApi } from '@/api/breakingBadApi';
 import useCharaters from "@/characters/composables/useCharaters";
-const { characters, isLoading, hasError, errorMessage } = useCharaters();
+import { useQuery } from '@tanstack/vue-query'
+import type { Character, Info, Result } from '../interfaces/characters';
+
+// (opcion 1) Uso mediante compsables fucntions 
+//const { characters, isLoading, hasError, errorMessage } = useCharaters();
+
+
+
+// (opcion 2) Mediante TanStack Query (VUE-QUERY)
+const getCharactersSlow = async (): Promise<Character> => {
+  const { data } = await rickAndMortyApi.get<Character>('/character')
+  return data
+}
+const { isLoading, isError, data: characters, error } = useQuery(
+  ['characters'],
+  getCharactersSlow,
+
+)
 
 </script>
 
 <template>
   <h1 v-if="isLoading">Loading...</h1>
   <ul>
-    <li v-for="{ id, name } of characters" :key="id">
+    <li v-for="{ id, name } of characters?.results" :key="id">
       {{ name }}
     </li>
   </ul>
 
-  <h5 v-if="hasError">Error: {{ errorMessage }}</h5>
-
+  <h5 v-if="isError">Error: {{ error }}</h5>
 </template>
 
 <style scoped></style>
--- 01-breakingBad/src/main.ts
+++ 01-breakingBad/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 import App from './App.vue';
 import router from './router';
 
@@ -6,7 +7,12 @@ import './assets/main.scss';
 
 const app = createApp(App);
 
+//puede manejar el cahce por nosotros
+//en algun lado de la memoria 
+//del navegador web va utilizar la 
+// ultima version actualizada. 
+app.use(VueQueryPlugin)
+
 
 app.use(router);
-
 app.mount('#app');
